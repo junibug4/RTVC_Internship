6357e7a1d537ec564d7e6089301bf4316b731a06
junibug4 <juniperdunn04@outlook.com>
2025-07-01 14:48:11 +0100

    Create sweep_set_values.ino

---
+++ sweep_set_values.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+
+Modified for this internship by junibug4 [30/6/25]
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+// twelve Servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+}
+
+void loop() {
+
+  for (int i=45; i<=135; i++){
+    myservo.write(i);
+    delay(5);}
+
+  for (int i=135; i>=45; i--){
+    myservo.write(i);
+    delay(5);}
+}
