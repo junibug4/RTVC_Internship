76319fcdde4fdce17090390fcdfb0bf97f1b3f84
junibug4 <juniperdunn04@outlook.com>
2025-07-03 16:08:57 +0100

    updating showFile.py

--- showFile.py
+++ showFile.py
@@ -1,7 +1,7 @@
 #%% ---- Imports ------------------------------------------------
 import matplotlib.pyplot as plt
 import numpy as np
-
+from scipy.optimize import curve_fit
 # %% ------ Plots the acceleration of the spinning rocket -------
 # ## --- Use this to calculate the rocket's moment of inertia ---
 
@@ -10,7 +10,7 @@ filename = 'datafiles/spin_fan15_04_1444.txt' # 'datafiles/spin_noFan_04_1413.tx
 data = np.loadtxt(filename)
 timeAxis = np.linspace(0,(len(data)*0.008),len(data)) # timestep for arduino is 8 ms
 
-start,end = 180,590
+start,end = 0,len(data)#180,590
 data = data[start:end]  # Remove first START and last END samples to remove initial noise
 timeAxis = timeAxis[start:end]  # Adjust time axis accordingly
 
@@ -21,14 +21,12 @@ timeAxis = timeAxis[start:end]  # Adjust time axis accordingly
 
 
 counter=0
-lasti = data[0]
 scaledData = np.zeros(len(data))
 for i in range (len(data[1:])):
-    if lasti-data[i] > 100:
+    if data[i-1]-data[i] > 100:
         counter += 1
         # print(timeAxis[i],counter)
     scaledData[i] = data[i] + counter*360
-    lasti = data[i]
 
 timeAxis, data = timeAxis[:-1], scaledData[:-1]
 
@@ -38,5 +36,24 @@ plt.xlabel('Time (s)')
 plt.ylabel('Displacement (degrees)')   
 plt.show()
 
+#%% ----------- Writing a fitting function ---------------------
 
-# %%
+def square(x, a, b=0, c=0):
+    return a*x**2 + b*x + c
+
+start = 190
+end = 590
+
+cutData = data[start:end]
+
+params, covariance = curve_fit(square, timeAxis[start:end], cutData)
+print(f"Fitted parameter: {params}")
+
+
+plt.plot(timeAxis, data, label='Data', alpha=0.6)
+plt.plot(timeAxis, square(timeAxis, *params), color='red', 
+         linestyle='dashed', label='Fitted Curve', alpha=0.7)
+plt.legend()
+plt.show()
+
+#%% ------------------------------------------------------------
